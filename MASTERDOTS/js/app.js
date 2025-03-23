9e63a86381a1574119ad457de707cba96556dece
Safaren <101393739+Safaren@users.noreply.github.com>
2025-03-23 20:34:08 +0100

    move files

--- MASTERDOTS/js/app.js
+++ MASTERDOTS/js/app.js
@@ -5,17 +5,26 @@
 const nickInput = document.getElementById("nick");
 const tamanoInput = document.getElementById("tamano");
 const InputForm = document.getElementById("inputForm");
-
+const error = document.getElementById("error");
 
 
 
 function checkform(event){
     //console.log("Comprobamos el formulario");
-    if (nickInput.value.length==0)
+    if(nickInput.value.match(/(?<!\S)[0-9]/))
     {
         console.log("No ha introducido su nick");
         nickInput.focus();
         event.preventDefault();
+        error.innerText = "Error dfds";
+        return false;
+    }
+    else if (nickInput.value.length==0)
+    {
+        console.log("No ha introducido su nick");
+        nickInput.focus();
+        event.preventDefault();
+        error.innerText = "Error";
         return false;
     } else if(tamanoInput.value=="0")
     {
